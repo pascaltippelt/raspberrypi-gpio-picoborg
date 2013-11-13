af7283e8113b8be402658632c31296620ebd2290
Pascal Tippelt <mail@pascaltippelt.de>
2013-11-13 15:17:57 +0100

    RELEASE 0.4.1
    Added automatic wiringPi install

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 sudo groupadd www-data
 sudo usermod -a -G www-data www-data
 sudo apt-get update
-sudo apt-get install apache2 php5 libapache2-mod-php5 libapache2-mod-perl2 php5 php5-cli php5-common php5-curl php5-dev php5-gd php5-imap php5-ldap php5-mhash php5-mysql php5-odbc php-pear php-apc
+sudo apt-get install apache2 php5 libapache2-mod-php5 libapache2-mod-perl2 php5 php5-cli php5-common php5-curl php5-dev php5-gd php5-imap php5-ldap php5-mhash php5-mysql php5-odbc php-pear php-apc git
 sudo chown pi:pi /var/www
 sudo chown pi:pi /var/www/*
 cp * /var/www
@@ -13,6 +13,12 @@ sudo echo "gpio -g mode 8 out" >> /bin/setgpio
 sudo echo "gpio -g mode 18 out" >> /bin/setgpio
 sudo chmod +x /bin/setgpio
 sudo echo "sudo -u www-data setgpio" >> /etc/rc.local
+echo "Now wiringPi will be installed. If fails, please install manual!"
+cd
+git clone git://git.drogon.net/wiringPi
+cd wiringPi
+./build
+gpio -v
 echo "Done. Now open [Adress od your Pi]/index.php ind a browser"
 echo "NEW ind version 0.4: Authentication. User: gpio Password: gpio . Feel free to change this in login.php."
 exit
